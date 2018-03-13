95033d622bb4dcdf54b9bcdfb76e76eeccdf42ad
Sara Brandes <smbrandes17@gmail.com>
2018-03-13 10:34:47 -0700

    added yelp api route for title-map

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const parse = require('body-parser');
 const route = require('./routes');
-
+const yelp = require('yelp-fusion');
 require('dotenv').config();
+
+const client = yelp.client(process.env.APIKEY);
 
 const app = express();
 
@@ -14,6 +16,15 @@ app.set('view engine', 'pug');
 app.use(express.static(`${__dirname}/../client`));
 
 app.use('/', route);
+
+app.get('/yelp/:term', (req, res) => {
+  const { term } = req.params;
+  client.search({
+    term,
+    location: 'san francisco, ca',
+  }).then(response => res.send(response.jsonBody.businesses[0]))
+    .catch((e) => { throw new Error(e); });
+});
 
 const port = process.env.PORT || 8000;
 
